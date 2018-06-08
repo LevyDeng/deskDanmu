3d66aab5cb4cb248f47d2ada1af81a3ffc21c4be
levi <levi@exabeam.com>
2018-06-08 18:56:36 +0800

    some fix

--- src/main/java/com/lightingcontour/toucher/MainService.java
+++ src/main/java/com/lightingcontour/toucher/MainService.java
@@ -1,5 +1,6 @@
 package com.lightingcontour.toucher;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -68,19 +69,19 @@ public class MainService extends Service {
         createToucher();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void createToucher()
     {
-
 
         //赋值WindowManager&LayoutParam.
         params = new WindowManager.LayoutParams();
         windowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
         //设置type.系统提示型窗口，一般都在应用程序窗口之上.
-        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;//| WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;;
         //设置效果为背景透明.
         params.format = PixelFormat.RGBA_8888;
         //设置flags.不可聚焦及不可使用按钮对悬浮窗进行操控.
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;//WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL|WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS|WindowManager.LayoutParams.FLAG_SPLIT_TOUCH;
 
         //设置窗口初始停靠位置.
         params.gravity = Gravity.START | Gravity.TOP;
@@ -88,12 +89,18 @@ public class MainService extends Service {
         params.y = 0;
 
         //设置悬浮窗口长宽数据.
-        params.width = 300;
-        params.height = 300;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = 780;
 
         LayoutInflater inflater = LayoutInflater.from(getApplication());
         //获取浮动窗口视图所在布局.
         toucherLayout = (ConstraintLayout) inflater.inflate(R.layout.toucherlayout,null);
+        toucherLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
         //添加toucherlayout
         windowManager.addView(toucherLayout,params);
 
@@ -135,15 +142,6 @@ public class MainService extends Service {
             }
         });
 
-        imageButton1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                params.x = (int) event.getRawX() - 150;
-                params.y = (int) event.getRawY() - 150 - statusBarHeight;
-                windowManager.updateViewLayout(toucherLayout,params);
-                return false;
-            }
-        });
 
         danmakuView = (DanmakuView) toucherLayout.findViewById(R.id.danmakuView);
         danmakuView.enableDanmakuDrawingCache(true);
@@ -172,6 +170,29 @@ public class MainService extends Service {
         });
         danmakuContext = DanmakuContext.create();
         danmakuView.prepare(parser, danmakuContext);
+        danmakuView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+
+        imageButton1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //params.x = (int) event.getRawX() - 75;
+                //params.y = (int) event.getRawY() - 75 - statusBarHeight;
+                params.height=(int) event.getRawY() + 150;
+                windowManager.updateViewLayout(toucherLayout,params);
+                v.setX((int) event.getRawX() - 225);
+                v.setY((int) event.getRawY() - 225 - statusBarHeight);
+                ConstraintLayout.LayoutParams danmuParams = (ConstraintLayout.LayoutParams) danmakuView.getLayoutParams();
+                danmuParams.height=(int) event.getRawY() - 150;
+                danmakuView.setLayoutParams(danmuParams);
+                return false;
+            }
+        });
 
     }
 
